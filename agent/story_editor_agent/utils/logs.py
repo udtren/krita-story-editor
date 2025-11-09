d544adabc37f91f4cd05d30f0f1320ce9f6d7a60
udtren <udtren@users.noreply.github.com>
2025-11-09 12:18:42 +0900

    update

--- agent/story_editor_agent/utils/logs.py
+++ agent/story_editor_agent/utils/logs.py
@@ -1,7 +1,13 @@
-def write_log(log_msg):
-    with open(
-        r"C:\Users\udtren\Project\krita-story-editor\agent\story_editor_agent\logs\log.txt",
-        "a",
-        encoding="utf-8",
-    ) as f:
-        f.write(log_msg + "\n")
+def write_log(log_msg, enable_debug=False):
+
+    enable_debug = True
+
+    if enable_debug:
+        with open(
+            r"C:\Users\udtren\Project\krita-story-editor\agent\story_editor_agent\logs\log.txt",
+            "a",
+            encoding="utf-8",
+        ) as f:
+            f.write(log_msg + "\n")
+    else:
+        pass
